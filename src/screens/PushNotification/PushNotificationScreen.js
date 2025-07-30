6150b966a8a99712022e87f3e56e730a2a41c4a2
Serdar Dev <serdardev2@gmail.com>
2025-07-30 17:05:33 +0300

    A .env.example file was added for environment variables, and the .env file was included in .gitignore. Additionally, the react-native-config dependency was added to the package.json file. Modular headers usage was enabled in the iOS Podfile, and a Podfile.lock file was created. API calls in the PushNotificationScreen component were updated to use environment variables.

--- src/screens/PushNotification/PushNotificationScreen.js
+++ src/screens/PushNotification/PushNotificationScreen.js
@@ -17,6 +17,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import RNPickerSelect from 'react-native-picker-select';
 import {getFcmToken, getCurrentFcmToken} from '../../notificationServices';
 import {Colors} from '../../constants/colors';
+import Config from 'react-native-config';
 
 const PushNotificationScreen = ({route}) => {
   const [fcmToken, setFcmToken] = useState('');
@@ -70,9 +71,8 @@ const PushNotificationScreen = ({route}) => {
     const requestData = {
       v: 1,
       platform: 'app',
-      admmdlid:
-        '12f3894ed72fc7d4e3b98688b20513e20a3fa1adbd08b9662412322138d26533',
-      scope: '8fbff85cb7a2b8cbd53b3086c0b16d4c1e96a5d748cbf8761bace32ab294e83a',
+      admmdlid: Config.API_ADMMDLID,
+      scope: Config.API_SCOPE,
       fcm_token: fcmToken,
       pn_type: pnTypeNumber,
       pn_delay: parseInt(delay),
